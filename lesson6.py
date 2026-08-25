e1f4d10b88dc6a8202656e8522f1cb50ed4217c6
AM-anathi <mtshawuanathi@gmail.com>
2026-08-25 18:16:24 +0200

    Add files via upload

---
+++ lesson6.py
@@ -0,0 +1,25 @@
+#collections 
+
+# (1) list is an order of collected items wrapped inside a square brackets []
+#cart = ["Apples", "Eggs", "Milk"]
+#print(cart[0]) # prints the 1st item in the cart list
+
+#cart.append("Bread")
+#print(cart)
+
+#for item in cart :
+ #   print(item)
+
+#Working with dictionaries , work with key value pairs 
+# 1st is the key
+# 2nd is the value pair 
+
+
+student = {
+    "name": "Thabo", 
+    "age": 25,
+    "course": "Intro to Python"
+}
+
+print(student["name"])
+print(student["age"])

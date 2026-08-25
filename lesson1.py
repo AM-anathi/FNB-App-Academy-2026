af1c7ed80268cc7da72a036084f08a4730940cd3
AM-anathi <mtshawuanathi@gmail.com>
2026-08-25 18:14:16 +0200

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,11 @@
+# Making the computer speak (making a comment)
+#print("Hello World!")
+
+# Stroing data inside a variable 
+#name = "Anathi " 
+#print (name) #output is Anathi
+#print("name") #output is name not Anathi
+
+#Making the code interactive 
+#name = input("Enter your name: ") # input functions allows the user to enter data into the program
+#print ( f"Greetings, {name}") # f - string allows us to insert variables into a string, { name} is a placeholder for the variable

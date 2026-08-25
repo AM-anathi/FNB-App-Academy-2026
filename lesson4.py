718d6cd5c8644cbec1c6c15fdd716cf0bfbed4aa
AM-anathi <mtshawuanathi@gmail.com>
2026-08-25 18:15:42 +0200

    Add files via upload

---
+++ lesson4.py
@@ -0,0 +1,26 @@
+# Basic if/else statement script 
+
+#age = int(input("Enter your age: "))
+#scetion_pass= input("Do you has a vip ticket ? (yes/no)").lower()
+
+#if age >= 18 and scetion_pass == "yes":   
+   # print("Access granted to the vip section")
+
+#elif age >= 18 :   #to add more choices 
+ #   print("Access granted to the genaral section !!")
+#else: 
+ #   print("Access denied !!")
+ 
+#Yt 
+#list mutable, values can be changed 
+#courses= ['History', 'Math','Physics', 'CompSci']
+#num
+#courses.sort() # alphbetic order 
+#courses.reverse()
+#popped = courses.pop()
+#courses_2 =['Art', 'Education']
+#courses.extend(courses_2) # appends the list itself 
+#print(popped)
+#print(courses)
+
+#tuples 

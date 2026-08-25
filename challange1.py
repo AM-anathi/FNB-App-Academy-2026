6ed0e5cd2679bae1bf3981cce9bec9f4fdfcfc76
AM-anathi <mtshawuanathi@gmail.com>
2026-08-25 18:16:36 +0200

    Add files via upload

---
+++ challange1.py
@@ -0,0 +1,5 @@
+#The concert Ticket Booker 
+ 
+name = input("Enter your name: ")
+artist = input("Enter the artist you want to see: ")
+print(f"Hey {name}! Your ticket to see {artist} are booked successfully !")
